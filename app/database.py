a8eeed08887cb23b7bb74ac5abf951379539168d
C0nst4nta <zeinollamagzhan@gmail.com>
2024-12-30 15:57:28 +0500

    database_url second fix

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@postgres-db:5433:5432/fastapi_db"
+DATABASE_URL = "postgresql://admin:admin123@postgres-db:5432/fastapi_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
